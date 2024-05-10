acaee540142c8068683d119142c27ec346208c88
Steve Nardone <sn@true.market>
2024-05-09 22:29:53 -0400

    Reorder authRoutes after cookie/passport initialization, include user in greeting.

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -3,24 +3,27 @@ import passport from 'passport'
 export function authRoutes(app) {  //
     app.get(
         '/',
-        (req,res) => {res.send('Hello World')}
+        (req,res) => {
+            res.send(`Hello ${req.user?.name ?? 'Unknown!'}`)
+        }
     )
-    
+
     app.get(
         '/auth/google',
         // authenticate using google strategy and request access to profile and email
         passport.authenticate('google',{ scope: ['profile', 'email'] } )
     )
 
+
     app.get(
         '/auth/google/callback',
-        // authenticate using google strategy     
+        // authenticate using google strategy
         //passport.authenticate('google', {session: false, failureRedirect: '/'})
-        passport.authenticate('google')
+        passport.authenticate('google', { successRedirect: '/', failureRedirect: '/fail' })
     )
 
     app.get(
-        '/api/current_user',     
+        '/api/current_user',
         (req, res) => {
             res.send(req.user)
         }
--- src/index.js
+++ src/index.js
@@ -10,7 +10,6 @@ import {keys} from '../config/keys.js'
 mongoose.connect(keys.mongoURI)
 
 const app = express()
-authRoutes(app)
 
 // enable cookies with 30 day life and encrypt with our "cookieKey"
 app.use(
@@ -22,6 +21,9 @@ app.use(
 
 app.use(passport.initialize())
 app.use(passport.session())
+
+// endpoint routes need to come after cookie/passport initialization
+authRoutes(app)
 
 const PORT = process.env.PORT || 5000
 console.log('Listening on ......',PORT)
